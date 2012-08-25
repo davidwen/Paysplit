f212c6cb08f5f8e58096487af3e1aa64c424c1bd
David Wen <david@dwen.me>
2012-08-25 15:50:12 -0400

    Add emails to send

--- app/controllers/Expenses.java
+++ app/controllers/Expenses.java
@@ -76,7 +76,7 @@ public class Expenses extends Controller {
         checkErrorsWithRollback();
         Set<String> dueUsernames = Sets.newHashSet();
 
-        Set<String> emails = Sets.newHashSet();
+        Set<String> emails = Sets.newHashSet(expenseUser.email, user.email);
         List<Due> dues = Lists.newArrayList();
         while(params.get(dueUsername + String.valueOf(iteration)) != null) {
             String fromUsername = params.get(dueUsername + String.valueOf(iteration));
@@ -119,6 +119,7 @@ public class Expenses extends Controller {
             checkErrorsWithRollback();
             Due.saveDue(due);
             dues.add(due);
+            emails.add(due.fromUser.email);
             iteration++;
         }
         if (totalDues.doubleValue() != expense.amount) {
