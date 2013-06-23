dc92b74aebbc1246564a7dd47c02876d56e800ba
David Wen <david@dwen.me>
2013-06-23 11:32:04 -0400

    Initial balances and fix user

--- app/controllers/Balances.java
+++ app/controllers/Balances.java
@@ -13,13 +13,24 @@ import play.mvc.Controller;
 public class Balances extends Controller{
 
     public static void showBalances() {
+        fillBalancesArgs();
+        render();
+    }
+
+    public static void mShowBalances() {
+        fillBalancesArgs();
+        render();
+    }
+
+    private static void fillBalancesArgs() {
         User user = User.fromSession(session);
         List<Balance> positiveBalances = Balance.getPositiveBalances(user);
         List<Balance> negativeBalances = Balance.getNegativeBalances(user);
         for (Balance negativeBalance : negativeBalances) {
             negativeBalance.balance = -1 * negativeBalance.balance;
         }
-        render(user, positiveBalances, negativeBalances);
+        renderArgs.put("user", user);
+        renderArgs.put("positiveBalances", positiveBalances);
+        renderArgs.put("negativeBalances", negativeBalances);
     }
-
 }
